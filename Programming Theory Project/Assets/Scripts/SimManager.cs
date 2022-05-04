8fcab19d0ca8ce214b9e22193c708dd6975d91ce
cmattes <coreymattes@gmail.com>
2022-05-04 01:45:20 -0500

    Updated the spawn manager
    spawn manager has some logic to spawn new shapes, need to fix an error with instantiating them

--- Programming Theory Project/Assets/Scripts/SimManager.cs
+++ Programming Theory Project/Assets/Scripts/SimManager.cs
@@ -49,17 +49,119 @@ public class SimManager : MonoBehaviour
 
     private void SpawnShape()
     {
+        int spawnVertices = DetermineSpawnVertices();
 
+        float spawnSize = DetermineSpawnSize();
+
+        if (spawnVertices >= 8)
+        {
+            SpawnCube(spawnSize);
+            return;
+        }
+
+        if(spawnVertices <= 0)
+        {
+            SpawnSphere(spawnSize);
+            return;
+        }
+
+        SpawnSphube(spawnVertices, spawnSize);
+    }
+
+    private int DetermineSpawnVertices()
+    {
+        int spawnVertices = referenceShape.vertices + 2;
+        
+        if (spawnerShape.vertices >= referenceShape.vertices)
+        {
+            spawnVertices = spawnerShape.vertices + 1;
+        }
+
+        if (spawnerShape.size <= referenceShape.size)
+        {
+            spawnVertices -= 1;
+        }
+
+        if (spawnerShape.spawnRate - spawnerShape.TimesSpawned == 1)
+        {
+            spawnVertices += 1;
+        }
+
+        return spawnVertices;
+    }
+
+    private float DetermineSpawnSize()
+    {
+        float spawnSize = referenceShape.size + 1;
+
+        if (spawnerShape.size <= referenceShape.size)
+        {
+            spawnSize = spawnerShape.size / 2;
+        }
+
+        if (spawnerShape.spawnRate - spawnerShape.TimesSpawned == 1)
+        {
+            spawnSize = spawnerShape.size + 1.5f;
+        }
+
+        return spawnSize;
+    }
+
+    private void SpawnCube(float spawnSize)
+    {
+        int spawnSpawnRate = 2;
+
+        if (spawnSize > 3)
+        {
+            spawnSpawnRate = 1;
+        }
+
+        Shape cube = new Cube(spawnSize, spawnSpawnRate);
+
+        Instantiate(cube, spawnerShape.transform.position + Vector3.up, referenceShape.transform.rotation);
+    }
+
+    private void SpawnSphere(float spawnSize)
+    {
+        int spawnSpawnRate = 3;
+
+        if (spawnSize < 0.5)
+        {
+            spawnSpawnRate = 5;
+        }
+        else if (spawnSize < 1)
+        {
+            spawnSpawnRate = 4;
+        }
+
+        Shape sphere = new Sphere(spawnSize, spawnSpawnRate);
+
+        Instantiate(sphere, spawnerShape.transform.position + Vector3.up, referenceShape.transform.rotation);
+    }
+
+    private void SpawnSphube(int spawnVertices, float spawnSize)
+    {
+        int spawnSpawnRate = 3;
+
+        if (spawnVertices >= 6 || spawnSize > 3)
+        {
+            spawnSpawnRate = 2;
+        }
+
+        Shape sphube = new Sphube(spawnVertices, spawnSize, spawnSpawnRate);
+
+        Instantiate(sphube, spawnerShape.transform.position + Vector3.up, referenceShape.transform.rotation);
     }
 
     IEnumerator PrepForSpawn()
     {
         yield return new WaitForSeconds(3);
 
+        SpawnShape();
+
         spawnerShape.selectedShapeIndicator.SetActive(false);
         referenceShape.selectedShapeIndicator.SetActive(false);
 
-        SpawnShape();
         spawnerShape.TimesSpawned++;
         SpawnerSelected = false;
     }
